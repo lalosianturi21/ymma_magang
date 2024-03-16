8d3bfbbd0369b91c21365c80174c16a099b245ad
swordxhunter <fulalotio210603@gmail.com>
2024-03-16 19:56:24 +0700

    first commit

---
+++ resources/views/artikel/template/app.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+{{-- head --}}
+    @include('sb-admin/head')
+
+  <body id="page-top">
+    {{-- navbar --}}
+    @include('artikel/template/navbar')
+
+    <div class="mx-auto" >
+        @yield('content')
+    </div>
+
+    <!-- Footer -->
+      @include('sb-admin/footer')
+
+    <!-- Scroll to Top Button-->
+    @include('sb-admin/button-topbar')
+
+   {{-- logout --}}
+   @include('sb-admin/logout-modal')
+  
+   {{-- javascript --}}
+   @include('sb-admin/javascript')
+  </body>
+ 
+
+</html>
